d60cb2b93e426c1894305904f6d1e0bcc1c174b0
Ignat Shcheglovskyi <ignat.scheglovskiy@gmail.com>
2013-01-23 22:44:28 +0200

    Updated phpunit.bootstrap.php, removed clearing cache routine

--- app/phpunit.bootstrap.php
+++ app/phpunit.bootstrap.php
@@ -4,9 +4,3 @@ require_once 'bootstrap.php.cache';
 
 echo 'Prepare mongodb fixtures...', PHP_EOL;
 system(sprintf('php %s/console doctrine:mongodb:fixtures:load --env test', __DIR__));
-
-register_shutdown_function(
-    function () {
-        system(sprintf('php %s/console cache:clear --env test', __DIR__));
-    }
-);
